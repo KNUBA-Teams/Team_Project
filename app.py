5792c52ac9f237993e0fb4bab9d8192cb72c7b3f
ssj <dk04188@naver.com>
2022-06-14 16:24:38 +0900

    sj_web_model

--- app.py
+++ app.py
@@ -4,11 +4,32 @@ import datetime
 import folium
 import json
 from folium import Marker
+from keras.models import load_model
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'd2707fea9778e085491e2dbbc73ff30e'
 
+def str_2words(word):
+    word = str(word)
+    if len(word)==1:
+        word='0'+word
+    return word
+
 start_coords = [35.8, 128.6]
+before_winter = 593
+region_fact = ['동인동', '삼덕동', '성내1동', '성내2동', '성내3동', '대신동', '남산1동', '남산2동', '남산3동', '남산4동', '대봉1동', '대봉2동',
+ '신암1동', '신암2동', '신암3동', '신암4동', '신암5동', '신천1.2동', '신천3동', '신천4동', '효목1동', '효목2동', '도평동', '불로.봉무동',
+ '지저동', '동촌동', '방촌동', '해안동', '안심1동', '안심2동', '안심3동', '안심3.4동', '안심4동', '혁신동', '공산동', '내당1동', '내당2.3동',
+ '내당4동', '비산1동', '비산2.3동', '비산4동', '비산5동', '비산6동', '비산7동', '평리1동', '평리2동', '평리3동', '평리4동', '평리5동',
+ '평리6동', '상중이동', '원대동', '이천동', '봉덕1동', '봉덕2동', '봉덕3동', '대명1동', '대명2동', '대명3동', '대명4동', '대명5동', '대명6동',
+ '대명9동', '대명10동', '대명11동', '고성동', '칠성동', '침산1동', '침산2동', '침산3동', '산격1동', '산격2동', '산격3동', '산격4동', '대현동',
+ '복현1동', '복현2동', '검단동', '무태조야동', '관문동', '태전1동', '태전2동', '구암동', '관음동', '읍내동', '동천동', '노원동', '국우동',
+ '범어1동', '범어2동', '범어3동', '범어4동', '만촌1동', '만촌2동', '만촌3동', '수성1가동', '수성2.3가동', '수성4가동', '황금1동', '황금2동',
+ '중동', '상동', '파동', '두산동', '지산1동', '지산2동', '범물1동', '범물2동', '고산1동', '고산2동', '고산3동', '성당동', '두류1.2동', '두류3동',
+ '감삼동', '죽전동', '장기동', '용산1동', '용산2동', '이곡1동', '이곡2동', '신당동', '본리동', '월성1동', '월성2동', '진천동', '상인1동',
+ '상인2동', '상인3동', '도원동', '송현1동', '송현2동', '본동', '화원읍', '논공읍', '다사읍', '유가읍', '옥포읍', '현풍읍', '가창면', '하빈면',
+ '구지면']
+model = load_model('model/model_2.0546_2.0417.h5')
 
 def get_dropdown_values():
     class_entry_relations = {
@@ -44,9 +65,52 @@ def update_dropdown():
 
 @app.route('/_process_data')
 def process_data():
-    selected_class = request.args.get('selected_class', type=str)
-    selected_entry = request.args.get('selected_entry', type=str)
-    return jsonify(random_text="선택한 시군구: {} and 선택한 읍면동: {}".format(selected_class, selected_entry))
+	selected_entry = request.args.get('selected_entry', type=str)
+	
+	df = pd.read_csv('./output/Daegu_data.csv')
+	df.drop(['long','lat'], axis=1, inplace=True)
+	df = df[df['region']==selected_entry]
+	df['region'] = region_fact.index(selected_entry)
+
+	df = df.iloc[-73:,:]
+	column_list = []
+
+	for i in range(1, 73):
+		for j in ['temp','rainfall','humidity','wind_speed','wind_direction']:
+			col_temp = 'next_' + j + '_h' + str(i)
+			column_list.append(col_temp)
+			df[col_temp] = df[j].shift(i)
+	df.dropna(inplace=True)
+	y = model.predict(df)
+
+	temps = list(y[0])
+	
+	now_hour = df.iloc[0,3]
+	list_temp = []
+	if now_hour == 23:
+		now_hour = -1
+	hour72 = 0
+	for i in range(24):
+		list_temp.append(str(i)+'h\t')
+	for i in range(96):
+		if now_hour >= 0:
+			list_temp[i%24] = list_temp[i%24] + '\t\t'
+			now_hour = now_hour - 1
+		elif hour72 < 72:
+			if i < 72:
+				list_temp[i%24] = list_temp[i%24] + str(temps[hour72]) + '\t'
+			else:
+				list_temp[i%24] = list_temp[i%24] + str(temps[hour72])
+			hour72 = hour72 + 1
+		else:
+			list_temp[i%24] = list_temp[i%24]
+
+	text = ''
+	for i in list_temp:
+		text += i
+		text += '\n'
+	
+	return jsonify(result = text)
 
 @app.route('/')
 def home():
@@ -69,7 +133,6 @@ def home():
 			Marker([lat, long], icon = folium.Icon(color='green'), popup=str(temp)+'℃ '+region).add_to(m)
 		else:
 			Marker([lat, long], icon = folium.Icon(color='blue'), popup=str(temp)+'℃ '+region).add_to(m)
-		
 
 	return render_template('layout.html', 
 							now_temp = now_temp, 
